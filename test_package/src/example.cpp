c55a40ea2aab5cda3c0bb791901ca4c6c33b15a1
Isaac Yuen <whyuenac@gmail.com>
2024-04-28 12:22:17 +0800

    include directory is not in build directory

--- test_package/src/example.cpp
+++ test_package/src/example.cpp
@@ -1,15 +1,9 @@
 #include "my_foundation/my_algorithm.h"
 #include <iostream>
-#include <vector>
-#include <string>
+
 
 int main() {
-//    my_algorithm();
-//
-//    std::vector<std::string> vec;
-//    vec.push_back("test_package");
-//
-//    my_algorithm_print_vector(vec);
+
     std::cout << "The answer is " << answer() << "\n";
 
 }
